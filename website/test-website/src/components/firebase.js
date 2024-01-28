08cdd7a03acc1b98242daadf6363193df207e62b
OlaAni <b00134084@mytudublin.ie>
2024-01-28 16:18:30 +0000

    fixed links, moved orpheus

--- website/test-website/src/components/firebase.js
+++ website/test-website/src/components/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { get, getDatabase } from "firebase/database";
+import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
